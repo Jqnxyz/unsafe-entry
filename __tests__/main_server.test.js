5f661db951c41c78c7e0f05d81a73900b8f9ce25
Jerome Quah <me@jqnxy.xyz>
2020-09-13 04:31:46 +0800

    Added new endpoints to tests

--- __tests__/main_server.test.js
+++ __tests__/main_server.test.js
@@ -1,13 +1,13 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
- 
+
 const app = require('../main_server.js')
 const supertest = require('supertest')
 const request = supertest(app)
 
 const endpointReturnCodes = [200, 302];
-const endpoint200 = ['/entry', '/qr'];
+const endpoint200 = ['/entry', '/qr', '/se/qr', '/se/config'];
 const endpoint302 = ['/'];
 
 const endpointReturnCodeArrays = [endpoint200, endpoint302];
