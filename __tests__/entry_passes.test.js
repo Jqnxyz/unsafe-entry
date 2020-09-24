853f5c069991c19712bb61964781fa4643a16934
Jerome Quah <me@jqnxy.xyz>
2020-09-24 23:07:38 +0800

    Added v3 test

--- __tests__/entry_passes.test.js
+++ __tests__/entry_passes.test.js
@@ -1,12 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
- 
+
 const app = require('../main_server.js')
 const supertest = require('supertest')
 const request = supertest(app)
 
-const passEntryVersions = ['v1', 'v2'];
+const passEntryVersions = ['v1', 'v2', 'v3'];
 
 passEntryVersions.forEach( function(element, index) {
   it('Testing pass entry endpoint ' + element, async done => {
